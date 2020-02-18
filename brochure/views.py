069335fdec1b0f42d77e89cf8fe42fa187779401
Luke Russell <thelukerussel21@gmail.com>
2020-02-18 12:46:01 -0500

    added templates

--- brochure/views.py
+++ brochure/views.py
@@ -3,3 +3,6 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'brochure/index.html'
+
+class AboutView(generic.TemplateView):
+    template_name = 'brochure/about.html'
